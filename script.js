6654b25befae5e0a3acbd26401b9d6067ad391dc
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-06-15 13:44:19 +0200

    changed names, return string

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 "use strict";
-let changeColor = document.querySelector(".changeColorOff");
+let background = document.querySelector(".backgroundOff");
 let button = document.querySelector("button");
-let touchTime = document.querySelector(".touchTime");
+let lastTime = document.querySelector(".lastTime");
 
 function turnOff() {
   button.textContent = "TURN OFF";
   let time = localStorage.getItem("timeOff");
-  touchTime.innerHTML = `Last turn off: ${formatDate(time)} `;
-  touchTime.style.color = "#7068ff";
+  lastTime.innerHTML = `Last turn off: ${formatDate(time)} `;
+  lastTime.style.color = "#7068ff";
 }
 
 function turnOn() {
   button.textContent = "TURN ON";
   let time = localStorage.getItem("timeOn");
-  touchTime.innerHTML = `Last turn on: ${formatDate(time)} `;
-  touchTime.style.color = "#121535";
+  lastTime.innerHTML = `Last turn on: ${formatDate(time)} `;
+  lastTime.style.color = "#121535";
 }
 
 if (localStorage.getItem("timeOff") !== null) {
@@ -36,39 +36,40 @@ button.addEventListener("click", () => {
   }
   button.classList.toggle("btnOff");
   button.classList.toggle("btnOn");
-  changeColor.classList.toggle("changeColorOff");
-  changeColor.classList.toggle("changeColorOn");
+  background.classList.toggle("backgroundOff");
+  background.classList.toggle("backgroundOn");
 });
 
 function formatDate(time) {
   const date = new Date(time);
   
-  let DD = date.getDate();
-   if (DD <10) { 
-    DD = "0"+ DD;
+  let day = date.getDate();
+   if (day <10) { 
+    day = "0"+ day;
   }
 
-  let MO = date.getMonth() + 1;
-  if (MO <10) { 
-    MO = "0"+ MO;
+  let month = date.getMonth() + 1;
+  if (month <10) { 
+    month = "0"+ month;
   }
 
-  let YYYY= date.getFullYear();
-  let HH = date.getHours();
-   if (HH <10) { 
-    HH = "0"+ HH;
+  let year= date.getFullYear();
+
+  let hour = date.getHours();
+   if (hour <10) { 
+    hour = "0"+ hour;
   }
 
-  let MI = date.getMinutes();
-   if (MI <10) { 
-    MI = "0"+ MI;
+  let minute = date.getMinutes();
+   if (minute <10) { 
+    minute = "0"+ minute;
   }
 
-  let SS = date.getSeconds();
-   if (SS <10) { 
-    SS = "0"+ SS;
+  let second = date.getSeconds();
+   if (second <10) { 
+    second = "0"+ second;
   }
 
-  let timeEnd = DD + "-" + MO + "-" + YYYY + "  " + HH + ":" + MI + ":" + SS;
-  return timeEnd;
+  return `${day} - ${month} -${year}  ${hour} : ${minute} : ${second}`;
+  
 }
